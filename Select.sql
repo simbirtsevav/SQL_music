b7094175ffc85f22efaab6eaff079053792d7db6
simbirtsevav <simbirtsevav@mail.ru>
2022-09-09 11:24:30 +0300

    ;

--- Select.sql
+++ Select.sql
@@ -1,18 +1,18 @@
 SELECT album_name, album_year FROM album
-WHERE album_year >=2018
+WHERE album_year >=2018;
 
 SELECT track_name FROM track
 ORDER BY track_duration DESC
-LIMIT 1
+LIMIT 1;
 
 SELECT track_name FROM track
-WHERE track_duration >=210
+WHERE track_duration >=210;
 
 SELECT collection_name FROM collection
-WHERE collection_year BETWEEN 2018 AND 2020
+WHERE collection_year BETWEEN 2018 AND 2020;
 
 SELECT musician_name FROM musician
-WHERE musician_name NOT LIKE '% %'
+WHERE musician_name NOT LIKE '% %';
 
 SELECT track_name FROM track
-WHERE track_name LIKE '%my%'
+WHERE track_name LIKE '%my%';
